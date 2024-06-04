e3d88debf3eb2d00c353a5615a2323d238595392
James McManus <james.mcmanus@kellogg.northwestern.edu>
2024-06-04 22:44:06 +0000

    Fixed username issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["username"] = params["username"],
-    @user["email"] = params["email"],
+    @user["username"] = params["username"]
+    @user["email"] = params["email"]
     @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
     redirect_to "/login"
