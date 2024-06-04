fc475068009b0f551143781dccb39eabcc65e25f
James McManus <james.mcmanus@kellogg.northwestern.edu>
2024-06-04 06:18:41 +0000

    Added in authorization, still need some authentication tweaks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,25 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by({ "email" => params["email"] })
-    if @user
+    if @user !=nil
       if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
         flash["notice"] = "You've logged in."
         redirect_to "/places"
       else
         flash["notice"] = "Unsuccessful login."
-        redirect_to "/sessions/new"
+        redirect_to "/login"
       end
     else
       flash["notice"] = "Unsuccessful login."
-      redirect_to "/sessions/new"
+      redirect_to "/login"
     end
-  end
+end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
